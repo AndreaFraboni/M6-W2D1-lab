2d8d09e52ed29f54cfd5f04a4293078f30bbafcc
Andrea Fraboni <151090404+AndreaFraboni@users.noreply.github.com>
2026-03-03 16:13:24 +0100

    Starting Exercize1

---
+++ Assets/_Project/Scripts/UIController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoSingleton<UIController>
+{
+    public GameObject PauseGamePanel;
+    public GameObject GameOverPanel;
+    public GameObject LevelUP;
+
+    public void ShowGameOver()
+    {
+        GameOverPanel.SetActive(true);
+    }
+
+    public void HideGameOver()
+    {
+        GameOverPanel.SetActive(false);
+    }
+
+    public void ShowPausePanel()
+    {
+        PauseGamePanel.SetActive(true);
+    }
+
+    public void HidePausePanel()
+    {
+        PauseGamePanel.SetActive(false);
+    }
+    public void OpenSettingsPanel()
+    {
+
+    }
+}
